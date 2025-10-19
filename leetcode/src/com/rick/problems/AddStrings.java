4702011678ec4c0d8a020d27d1e950f96585e815
Rick <rick8220602@gmail.com>
2025-10-19 20:43:52 +0800

    Update AddStrings.java

--- leetcode/src/com/rick/problems/AddStrings.java
+++ leetcode/src/com/rick/problems/AddStrings.java
@@ -20,7 +20,7 @@ class AddStringsBruteForce {
             if (i <= len1) sum += num1.charAt(len1 - i) - '0';
             if (i <= len2) sum += num2.charAt(len2 - i) - '0';
             sb.append(sum % 10);
-            afd我是一行美麗的錯誤
+            我是一行美麗的錯誤
             carry = sum / 10;
         }
         sb = null;
