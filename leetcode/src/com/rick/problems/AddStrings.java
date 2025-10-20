c0b2600f6e80deb59118ffde80bb21d7306007f9
Rick <rick8220602@gmail.com>
2025-10-20 10:21:11 +0800

    Update AddStrings.java

--- leetcode/src/com/rick/problems/AddStrings.java
+++ leetcode/src/com/rick/problems/AddStrings.java
@@ -16,12 +16,8 @@ class AddStringsBruteForce {
         StringBuilder sb = new StringBuilder();
         int len1 = num1.length(), len2 = num2.length(), carry = 0;
         for (int i = 1; i <= Math.max(len1, len2) || carry > 0; i++) {
-            int sum = carry;
-            if (i <= len1) sum += num1.charAt(len1 - i) - '0';
-            if (i <= len2) sum += num2.charAt(len2 - i) - '0';
-            sb.append(sum % 10);
-            carry = sum / 10;
+
         }
-        return sb.reverse().toString();
+        return "";
     }
 }
